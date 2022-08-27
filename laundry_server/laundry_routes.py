6999128b105067003c7399c81c59d1a515969408
Joshua Smith <joshuasmith6556@gmail.com>
2022-08-27 15:48:18 -0700

    Add villages information to laundry api

--- laundry_server/laundry_routes.py
+++ laundry_server/laundry_routes.py
@@ -1,3 +1,4 @@
+import re
 from http import HTTPStatus
 
 import laundry_util
@@ -8,3 +9,40 @@ def get_hall_status(hall_id: str):
     machines = laundry_util.get_hall_machines(html)
 
     return machines, HTTPStatus.OK
+
+
+def get_halls():
+    villages = laundry_util.get_villages()
+
+    # Convert some special html codes to more common characters
+    char_convert_dict = {
+        '&#696;': '',
+        '&scaron;': 's',
+        '&#660;': '',
+        '&#322;': 'l',
+        '&#616;': 'i'
+    }
+
+    char_convert = lambda x: char_convert_dict[x.group()]
+
+    char_convert_pattern_string = f'({"|".join(char_convert_dict.keys())})'
+    char_convert_pattern = re.compile(char_convert_pattern_string)
+
+    result = {}
+    for village_url, village_name in villages:
+        halls = laundry_util.get_village_halls(village_url)
+        village_name = char_convert_pattern.sub(char_convert, village_name)
+
+        for hall_id, hall_name in halls:
+            if village_name == 'yakitutu':
+                hall_name = re.sub(r'Bldg \w - ', '', hall_name).strip()
+
+            hall_name = char_convert_pattern.sub(char_convert, hall_name)
+
+            result[hall_name] = {
+                'village': village_name,
+                'hallId': hall_id,
+                'hallName': hall_name
+            }
+
+    return result, HTTPStatus.OK
--- laundry_server/laundry_util.py
+++ laundry_server/laundry_util.py
@@ -4,9 +4,56 @@ from typing import List, Tuple
 import requests
 
 
+def get_villages() -> List[Tuple[str]]:
+    """
+    Gets the url and name of each residential village in Cal Poly. Examples of
+    residential villages: Cerro Vista, North Mountain, etc.
+    """
+
+    directory = 'http://washalert.washlaundry.com' + \
+                '/washalertweb/calpoly'
+
+    url = f'{directory}/cal-poly.html'
+
+    req = requests.get(url)
+    assert req.status_code == 200, f'HTTP request received {req.status_code}'
+
+    html = req.text
+
+    # Extract table html from html
+    table_pattern = re.compile(r'(?:<table>)((.|\n)+?)(?:<\/table>)')
+    table_html = next(filter(lambda x: x.strip(), table_pattern.findall(html)[0]))
+
+    entry_pattern = re.compile(r'<td><a href="(.+?)">(.+?)<\/a><\/td>')
+    table_entries = entry_pattern.findall(table_html)
+
+    result = []
+    for href, village in table_entries:
+        village_url = f'{directory}/{href}'
+        village_name = re.sub(r'\s+', ' ', village)
+        result.append((village_url, village_name))
+
+    return result
+
+
+def get_village_halls(url: str) -> List[Tuple[str]]:
+    """
+    Get the halls in a village from the url.
+    """
+
+    req = requests.get(url)
+    assert req.status_code == 200, f'HTTP request received {req.status_code}'
+    html = req.text
+
+    hall_pattern = re.compile(r'<td><a href=".+?location=(.+?)">(.+?)<\/a><\/td>')
+    hall_matches = hall_pattern.findall(html)
+
+    return hall_matches
+
+
 def get_hall_html(hall_id: str) -> str:
     """
-    Gets HTML document from washalert
+    Gets HTML document from washalert for the hall with the given id
     """
 
     # Get html page
@@ -49,6 +96,9 @@ def get_hall_machines(html: str) -> List[Tuple[str]]:
 
 
 if __name__ == '__main__':
-    foxen_id = '6a7d18d3-6225-417d-874f-ddc72b878219'
-    html = get_hall_html(foxen_id)
-    print(get_hall_machines(html))
+    # foxen_id = '6a7d18d3-6225-417d-874f-ddc72b878219'
+    # html = get_hall_html(foxen_id)
+    # print(get_hall_machines(html))
+
+    villages = get_villages()
+    print(get_village_halls(villages[2][0]))
